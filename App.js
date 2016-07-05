2130cf1fc851596dbb991700aa8b36ea34a83caf
Vijai Rajpal <vrajpal@starbucks.com>
2016-07-05 16:25:43 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            txt: 'this is the state txt',
+            cat: 0 
+        }
+    }
+    update(e){
+        this.setState({txt: e.target.value})
+    }
+    render() {
+        return (
+            <div>
+                <h1> {this.state.txt} </h1>
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: "this is the default text"
+}
+
+ReactDOM.render(
+    <App cat={5} txt="this is the props value" />,
+    document.getElementById('app')
+);
