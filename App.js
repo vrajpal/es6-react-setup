0c5f3dcffd2c0d37336519e028b0813f1433df14
Vijai Rajpal <vrajpal@starbucks.com>
2016-07-12 14:52:54 -0700

    owner ownee relationship

--- App.js
+++ App.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor() {
         super();
-        this.state = { 
-            txt: 'this is the state txt',
-            cat: 0 
-        }
+        this.state = { txt: '' }
+        this.update = this.update.bind(this);
     }
     update(e){
         this.setState({txt: e.target.value})
@@ -14,26 +12,23 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <input type="text"
-                    onChange={this.update.bind(this)} />
-                <h1> {this.state.txt} </h1>
+                <Widget txt={this.state.txt} update={this.update} />
+                <Widget txt={this.state.txt} update={this.update} />
+                <Widget txt={this.state.txt} update={this.update} />
+                <Widget txt={this.state.txt} update={this.update} />
             </div>
         );
     }
 }
 
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
+const Widget = (props) => {
+    return (
+        <div>
+            <input type="text"
+                onChange={props.update} />
+            <h1> {props.txt} </h1>
+        </div>
+    );
 }
 
-App.defaultProps = {
-    txt: "this is the default text"
-}
-
-ReactDOM.render(
-    <App cat={5} txt="this is the props value" />,
-    document.getElementById('app')
-);
-
-export default App
+ReactDOM.render(<App />, document.getElementById('app'));
